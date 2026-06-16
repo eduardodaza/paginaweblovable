91105c5e7d0905fae2278e604934378a4a710cc5
eduardodaza <edward_Daza@hotmail.com>
2026-06-15 21:50:52 -0500

    Update _app.tsx

--- cuenta-test-viajes-main/viajes-main/src/pages/_app.tsx
+++ cuenta-test-viajes-main/viajes-main/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 // src/pages/_app.tsx
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
+import "@/styles/itinerary-light.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
